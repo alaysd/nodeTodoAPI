57b2aa1d7dcc4b15ad7d85554f3f51f8459ea904
Alay Dhagia <alay.dhagia@Simforms-209.local>
2018-01-12 14:24:41 +0530

    POST request routing mongoose data insert

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -16,6 +16,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
   }).then((result)=>{
     console.log(result);
   })
-
   db.close();
 });
---
+++ server/server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((result)=>{
+    res.send(result);
+  },(e)=>{
+    res.status(400).send(e);
+  })
+})
+
+app.post('/todos2',(req,res)=>{
+  var todo = new Todo({
+    text: req.body.text,
+    completed: req.body.completed,
+    completedAt: req.body.completedAt
+  });
+  todo.save().then((result)=>{
+    res.send(result);
+  },(err)=>{
+    res.status(400).send(err);
+  })
+})
+
+app.listen(3000,()=>{
+  console.log('Started on port');
+})
