f91992b85fce0ce1e23a9ae39e20de44f34dc8e2
Alay Dhagia <alay.dhagia@Simforms-209.local>
2018-01-12 18:07:56 +0530

    blah

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var bodyParser = require('body-parser')
 
@@ -33,71 +32,15 @@ app.post('/todos2',(req,res)=>{
     res.status(400).send(err);
   })
 })
-
+app.get('/todos',(req,res)=>{
+  Todo.find().then((result)=>{
+      res.send({result});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+})
 app.listen(3000,()=>{
   console.log('Started on port');
 })
-=======
-var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-var Todo = mongoose.model('Todo',{
-  text:{
-    type:String,
-    required: true,
-    minlength: 1,
-    trim:true
-  },
-  completed:{
-    type:Boolean,
-    default: false
-  },
-  completedAt:{
-    type:Number,
-    default:null
-  }
-});
-
-// var newTodo = new Todo({
-//     text:'  Hello world   '
-// });
-//
-// newTodo.save().then((result)=>{
-//   console.log(result);
-// },(err)=>{
-//   console.log(err);
-// });
-
-// var otherTodo = new Todo({
-//   text:'Alay Dhagia',
-//   completed: true,
-//   completedAt: 1234
-// });
-// otherTodo.save().then((result)=>{
-//   console.log(result);
-// },(err)=>{
-//   console.log(err);
-// });
-//save new something
-
-var User = mongoose.model('User',{
-  email:{
-    type:String,
-    required : true,
-    trim:true,
-    minlength:1
-  }
-
-});
-
-var user = new User({
-  email:'dhagiaalay@gmail.com'
-});
-user.save().then((result)=>{
-  console.log(result);
-},(err)=>{
-  console.log(err);
-});
->>>>>>> ed624b3aa94b2fcab049e7601eadd0c27c620ea4
+module.exports ={app};
