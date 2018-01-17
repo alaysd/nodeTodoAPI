18f7d43b48498a10879f737353c8dcb15f85320f
Alay Dhagia <alay.dhagia@Simforms-209.local>
2018-01-17 14:46:40 +0530

    For deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,4 +6,3 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 module.exports ={
   mongoose
 };
-
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -59,8 +60,8 @@ app.get('/todos/:id',(req,res)=>{
   })
 });
 
-app.listen(3000,()=>{
-  console.log('Started on port');
-})
+app.listen(port,()=>{
+  console.log(`started at port ${port}`);
+});
 
 module.exports ={app};
