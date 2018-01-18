cd707a76c7d1f1cf0d761c6c6f9926a6bc24bbe3
alaysd <dhagiaalay@gmail.com>
2018-01-18 18:42:55 +0530

    AUTH

--- server/server.js
+++ server/server.js
@@ -67,6 +67,7 @@ app.get('/todos/:id',authenticate,(req,res)=>{
 
 app.delete('/todos/:id',authenticate,(req,res)=>{
   // get the id
+//OO
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
