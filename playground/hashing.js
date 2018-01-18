a4642af0aa01cc555433b02983db9ec490551cb3
Alay Dhagia <alay.dhagia@Simforms-209.local>
2018-01-18 14:09:39 +0530

    hashing user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+//bcrupt same text gives different text everytime
+var password = '123abc';
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(password,salt,(err,hash)=>{
+//     console.log(hash);
+//   })
+// });
 
-var data ={
-  id:10
-}
+var hashedPassword = '$2a$10$8q26wRE.MIuRwmIkhRv/ueYpdCR1brRZT96Gz1zMtrWrcj3vP7EPa';
+//Fetch the hasedPassword value from the database then compare it with the basic text user gave us then BOOM xD
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+  console.log(res);
+});
 
-var token = jwt.sign(data,'123abc'); //take the data and signs it meaning hashes it and creates token
-console.log(token);
-var decoded =jwt.verify(token,'123abc') //takest the token and sees if the data was not manipulated
-console.log('Decoded:',decoded);
+
+// var data ={
+//   id:10
+// }
+//
+// var token = jwt.sign(data,'123abc'); //take the data and signs it meaning hashes it and creates token
+// console.log(token);
+// var decoded =jwt.verify(token,'123abc') //takest the token and sees if the data was not manipulated
+// console.log('Decoded:',decoded);
 //VERIFY Signature
 // HMACSHA256(
 //   base64UrlEncode(header) + "." +
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+//Mongoose middleware lets us run certain code after certain events. EG. we can run some code after we update document or before we update
 //We cannt add methods to model until we define a schema for model
 var UserSchema = new mongoose.Schema({
     email:{
@@ -69,6 +71,23 @@ UserSchema.statics.findByToken = function(token){
     'tokens.access':'auth'
   })
 };
+
+UserSchema.pre('save',function(next){
+  var user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hash)=>{
+        user.password = hash;
+        next();
+      })
+    })
+  }else{
+    next();
+  }
+
+
+});
 var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
